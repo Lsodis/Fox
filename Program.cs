1b2c96e685a3b4ac418357675e6d8de26b3de3fd
Vova Zelenyi <vova2010201041@gmail.com>
2022-05-16 13:27:43 +0300

    master

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Try{
+    class MainClass{   
+        
+  
+        static void Main(){        
+   
+
+            System.Console.WriteLine("Teest");
+
+
+
+
+
+        }
+    }   
+ }
